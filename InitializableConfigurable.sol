1ca494f35b691e7854c2fc79a737dcb4db13990b
s-finance <stablefinance@protonmail.com>
2020-09-04 02:28:48 +0800

    +SToken.sol and SFarming.sol

---
+++ InitializableConfigurable.sol
@@ -0,0 +1,185 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+/**
+ * @title Initializable
+ *
+ * @dev Helper contract to support initializer functions. To use it, replace
+ * the constructor with a function that has the `initializer` modifier.
+ * WARNING: Unlike constructors, initializer functions must be manually
+ * invoked. This applies both to deploying an Initializable contract, as well
+ * as extending an Initializable contract via inheritance.
+ * WARNING: When used with inheritance, manual care must be taken to not invoke
+ * a parent initializer twice, or ensure that all initializers are idempotent,
+ * because this is not dealt with automatically as with constructors.
+ */
+contract Initializable {
+
+  /**
+   * @dev Indicates that the contract has been initialized.
+   */
+  bool private initialized;
+
+  /**
+   * @dev Indicates that the contract is in the process of being initialized.
+   */
+  bool private initializing;
+
+  /**
+   * @dev Modifier to use in the initializer function of a contract.
+   */
+  modifier initializer() {
+    require(initializing || isConstructor() || !initialized, "Contract instance has already been initialized");
+
+    bool isTopLevelCall = !initializing;
+    if (isTopLevelCall) {
+      initializing = true;
+      initialized = true;
+    }
+
+    _;
+
+    if (isTopLevelCall) {
+      initializing = false;
+    }
+  }
+
+  /// @dev Returns true if and only if the function is running in the constructor
+  function isConstructor() private view returns (bool) {
+    // extcodesize checks the size of the code stored in an address, and
+    // address returns the current address. Since the code is still not
+    // deployed when running a constructor, any checks on its code size will
+    // yield zero, making it an effective way to detect if a contract is
+    // under construction or not.
+    address self = address(this);
+    uint256 cs;
+    assembly { cs := extcodesize(self) }
+    return cs == 0;
+  }
+
+  // Reserved storage space to allow for layout changes in the future.
+  uint256[50] private ______gap;
+}
+
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ *
+ * Source https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-solidity/v2.1.3/contracts/ownership/Ownable.sol
+ * This contract is copied here and renamed from the original to avoid clashes in the compiled artifacts
+ * when the user imports a zos-lib contract (that transitively causes this contract to be compiled and added to the
+ * build/artifacts folder) as well as the vanilla Ownable implementation from an openzeppelin version.
+ */
+contract InitializableOwnable  is Initializable {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    constructor () internal {
+        initialize(msg.sender);
+    }
+    
+    /**
+     * @dev Contract initializer.
+     * called once by the factory at time of deployment
+     */
+    function initialize(address owner) virtual public initializer {
+        _owner = owner;
+        emit OwnershipTransferred(address(0), _owner);
+    }
+
+    /**
+     * @return the address of the owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(isOwner());
+        _;
+    }
+
+    /**
+     * @return true if `msg.sender` is the owner of the contract.
+     */
+    function isOwner() public view returns (bool) {
+        return msg.sender == _owner;
+    }
+
+    /**
+     * @dev Allows the current owner to relinquish control of the contract.
+     * @notice Renouncing to ownership will leave the contract without an owner.
+     * It will not be possible to call the functions with the `onlyOwner`
+     * modifier anymore.
+     */
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0));
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+
+contract InitializableConfigurable is InitializableOwnable {
+
+    mapping (bytes32 => uint) internal config;
+    
+    function getConfig(bytes32 key) public view returns (uint) {
+        return config[key];
+    }
+    function getConfig(bytes32 key, uint index) public view returns (uint) {
+        return config[bytes32(uint(key) ^ index)];
+    }
+    function getConfig(bytes32 key, address addr) public view returns (uint) {
+        return config[bytes32(uint(key) ^ uint(addr))];
+    }
+
+    function _setConfig(bytes32 key, uint value) internal {
+        if(config[key] != value)
+            config[key] = value;
+    }
+    function _setConfig(bytes32 key, uint index, uint value) internal {
+        _setConfig(bytes32(uint(key) ^ index), value);
+    }
+    function _setConfig(bytes32 key, address addr, uint value) internal {
+        _setConfig(bytes32(uint(key) ^ uint(addr)), value);
+    }
+    
+    function setConfig(bytes32 key, uint value) external onlyOwner {
+        _setConfig(key, value);
+    }
+    function setConfig(bytes32 key, uint index, uint value) external onlyOwner {
+        _setConfig(bytes32(uint(key) ^ index), value);
+    }
+    function setConfig(bytes32 key, address addr, uint value) public onlyOwner {
+        _setConfig(bytes32(uint(key) ^ uint(addr)), value);
+    }
+}
