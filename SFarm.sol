3dfab84b673ae8a3bbb9a6908f5c0b102e323351
s-finance <stablefinance@protonmail.com>
2020-09-07 07:55:01 +0800

    +SCurvePool

--- SFarm.sol
+++ SFarm.sol
@@ -36,7 +36,10 @@ contract SStakingPool is ISPool, Governable {
 	uint public end;
 	uint public totalStaking;
 	mapping(address => uint) public stakingOf;
-	mapping(address => uint) public lasttimeOf;
+	mapping(address => uint) public sumRewardPerOf;
+	uint public sumRewardPer;
+	uint public bufReward;
+	uint public lasttime;
 	
 	constructor(address _farm, address _underlying) public {
 		initialize(msg.sender, _farm, _underlying);
@@ -58,6 +61,7 @@ contract SStakingPool is ISPool, Governable {
             end = now + _span;
         else
             end = 0;
+        lasttime = now;
     }
     
     function farming(uint amount) virtual override external {
@@ -103,27 +107,46 @@ contract SStakingPool is ISPool, Governable {
         return harvest(msg.sender);
     }
     function harvest(address to) virtual override public returns (uint[] memory amounts) {
-        amounts = harvestCapacity(msg.sender);
-        amounts = _harvest(to, amounts);
-    
-        lasttimeOf[msg.sender] = now;
-
-        emit Harvest(msg.sender, to, amounts);
-    }
-    function _harvest(address to, uint[] memory amounts) virtual internal returns (uint[] memory) {
-        if(amounts.length > 0 && amounts[0] > 0)
-            IFarm(farm).crop().safeTransferFrom(farm, to, amounts[0]);
-        return amounts;
-    }
-    
-    function harvestCapacity(address farmer) virtual override public view returns (uint[] memory amounts) {
+        amounts = new uint[](1);
+        amounts[0] = 0;
         if(span == 0 || totalStaking == 0)
             return amounts;
         
-        uint amount = IERC20(IFarm(farm).crop()).allowance(farm, address(this));
-        amount = amount.mul(stakingOf[farmer]).div(totalStaking);
+        uint delta = _harvestDelta();
+        amounts[0] = _harvestCapacity(msg.sender, delta, sumRewardPer, sumRewardPerOf[msg.sender]);
         
-        uint lasttime = lasttimeOf[farmer];
+        if(delta != amounts[0])
+            bufReward = bufReward.add(delta).sub(amounts[0]);
+        if(delta > 0)
+            sumRewardPer = sumRewardPer.add(delta.mul(1 ether).div(totalStaking));
+        if(sumRewardPerOf[msg.sender] != sumRewardPer)
+            sumRewardPerOf[msg.sender] = sumRewardPer;
+        lasttime = now;
+
+        _harvest(to, amounts);
+    
+        emit Harvest(msg.sender, to, amounts);
+    }
+    function _harvest(address to, uint[] memory amounts) virtual internal {
+        if(amounts.length > 0 && amounts[0] > 0)
+            IFarm(farm).crop().safeTransferFrom(farm, to, amounts[0]);
+    }
+    
+    function harvestCapacity(address farmer) virtual override public view returns (uint[] memory amounts) {
+        amounts = new uint[](1);
+        amounts[0] = _harvestCapacity(farmer, _harvestDelta(), sumRewardPer, sumRewardPerOf[farmer]);
+    }
+    function _harvestCapacity(address farmer, uint delta, uint sumPer, uint lastSumPer) virtual internal view returns (uint amount) {
+        if(span == 0 || totalStaking == 0)
+            return 0;
+        
+        amount = sumPer.sub(lastSumPer);
+        amount = amount.add(delta.mul(1 ether).div(totalStaking));
+        amount = amount.mul(stakingOf[farmer]).div(1 ether);
+    }
+    function _harvestDelta() virtual internal view returns(uint amount) {
+        amount = IERC20(IFarm(farm).crop()).allowance(farm, address(this)).sub(bufReward);
+
         if(end == 0) {                                                         // isNonLinear, endless
             if(now.sub(lasttime) < span)
                 amount = amount.mul(now.sub(lasttime)).div(span);
@@ -131,11 +154,106 @@ contract SStakingPool is ISPool, Governable {
             amount = amount.mul(now.sub(lasttime)).div(end.sub(lasttime));
         else if(lasttime >= end)
             amount = 0;
-            
-        amounts = new uint[](1);
-        amounts[0] = amount;
     }
 } 
+
+contract SCurvePool is SStakingPool {
+	address internal constant CRV = 0xD533a949740bb3306d119CC777fa900bA034cd52;
+	address public minter;
+	address public gauge;
+	address public reward;
+	mapping(address => uint) public sumReward2PerOf;
+	mapping(address => uint) public sumReward3PerOf;
+	uint public sumReward2Per;
+	uint public sumReward3Per;
+	
+	constructor(address _farm, address _underlying, address _minter, address _gauge, address _reward) SStakingPool(_farm, _underlying) public {
+		initialize(msg.sender, _farm, _underlying, _minter, _gauge, _reward);
+	}
+	
+	function initialize(address governor, address _farm, address _underlying, address _minter, address _gauge, address _reward) public initializer {
+	    super.initialize(governor, _farm, _underlying);
+	    
+	    minter = _minter;
+	    gauge  = _gauge;
+	    reward  = _reward;
+	    
+	    ICurveGauge(gauge).integrate_checkpoint();      // just check
+	    ICurveMinter(minter).token();                   // just check
+	    IERC20(reward).totalSupply();                   // just check
+	}
+    
+    function _farming(address from, uint amount) virtual override internal {
+        super._farming(from, amount);                   // underlying.safeTransferFrom(from, address(this), amount);
+        ICurveGauge(gauge).deposit(amount);
+    }
+
+    function _unfarming(address to, uint amount) virtual override internal returns (uint){
+        ICurveGauge(gauge).withdraw(amount);
+        super._unfarming(to, amount);                   // underlying.safeTransfer(to, amount);
+        return amount;
+    }
+    
+    function harvest(address to) virtual override public returns (uint[] memory amounts) {
+        uint amount = super.harvest(to)[0];
+        
+        if(reward == address(0))
+            amounts = new uint[](2);
+        else
+            amounts = new uint[](3);
+        
+        amounts[1] = 0;
+        amounts[0] = amount;
+        
+        if(span == 0 || totalStaking == 0)
+            return amounts;
+        
+        uint delta = IERC20(CRV).balanceOf(address(this));
+        ICurveMinter(minter).mint(gauge);
+        delta = IERC20(CRV).balanceOf(address(this)).sub(delta);
+        //uint delta = _harvestDelta2();
+        amounts[1] = _harvestCapacity(msg.sender, delta, sumReward2Per, sumReward2PerOf[msg.sender]);
+        
+        if(delta > 0)
+            sumReward2Per = sumReward2Per.add(delta.mul(1 ether).div(totalStaking));
+        if(sumReward2PerOf[msg.sender] != sumReward2Per)
+            sumReward2PerOf[msg.sender] = sumReward2Per;
+            
+        if(reward != address(0)) {
+            delta = IERC20(reward).balanceOf(address(this));
+            ICurveGauge(gauge).claim_rewards();
+            delta = IERC20(reward).balanceOf(address(this)).sub(delta);
+            amounts[2] = _harvestCapacity(msg.sender, delta, sumReward3Per, sumReward3PerOf[msg.sender]);
+            
+            if(delta > 0)
+                sumReward3Per = sumReward3Per.add(delta.mul(1 ether).div(totalStaking));
+            if(sumReward3PerOf[msg.sender] != sumReward3Per)
+                sumReward3PerOf[msg.sender] = sumReward3Per;
+        } else
+            amounts[2] = 0;
+
+        _harvest2(to, amounts);
+    
+        emit Harvest(msg.sender, to, amounts);
+    }
+    function _harvest2(address to, uint[] memory amounts) virtual internal {
+        if(amounts.length > 1 && amounts[1] > 0)
+            CRV.safeTransfer(to, amounts[1]);
+            
+        if(amounts.length > 2 && amounts[2] > 0 && reward != address(0))
+            reward.safeTransfer(to, amounts[2]);
+    }
+
+    function harvestCapacity(address farmer) virtual override public view returns (uint[] memory amounts) {
+        amounts = new uint[](2);
+        amounts[0] = _harvestCapacity(farmer, _harvestDelta(),  sumRewardPer,  sumRewardPerOf[farmer]);
+        amounts[1] = _harvestCapacity(farmer, _harvestDelta2(), sumReward2Per, sumReward2PerOf[farmer]);
+    }    
+    function _harvestDelta2() virtual internal view returns(uint amount) {
+        amount = ICurveGauge(gauge).claimable_tokens(address(this));
+    }
+}
+
 
 contract SFarm is IFarm, Governable {
     using TransferHelper for address;
@@ -155,6 +273,24 @@ contract SFarm is IFarm, Governable {
         crop.safeApprove(pool, amount);
     }
     
+}
+
+
+interface ICurveGauge {
+    function deposit(uint _value) external;
+    function deposit(uint _value, address addr) external;
+    function withdraw(uint _value) external;
+    function withdraw(uint _value, bool claim_rewards) external;
+    function claim_rewards() external;
+    function claim_rewards(address addr) external;
+    function claimable_reward(address addr) external view returns (uint);
+    function claimable_tokens(address addr) external view returns (uint);
+    function integrate_checkpoint() external view returns (uint);
+}
+
+interface ICurveMinter {
+    function token() external view returns (address);
+    function mint(address _gauge) external;
 }
 
 
