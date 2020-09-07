c4e1f872f75cbedd392bcaf2ecf97c2f78c485f6
s-finance <stablefinance@protonmail.com>
2020-09-07 16:05:40 +0800

    +TestGauge

--- SFarm.sol
+++ SFarm.sol
@@ -293,7 +293,7 @@ interface ICurveMinter {
 
 
 contract SCurvePool is SExactPool, ICurveGauge {
-	address internal constant CRV = 0xD533a949740bb3306d119CC777fa900bA034cd52;
+	address public CRV;
 	address public minter;
 	address public gauge;
 	address public reward;
@@ -309,12 +309,12 @@ contract SCurvePool is SExactPool, ICurveGauge {
 	function initialize(address governor, address _farm, address _underlying, address _minter, address _gauge, address _reward) public initializer {
 	    super.initialize(governor, _farm, _underlying);
 	    
-	    minter = _minter;
-	    gauge  = _gauge;
+	    minter  = _minter;
+	    gauge   = _gauge;
 	    reward  = _reward;
 	    
+	    CRV = ICurveMinter(minter).token();
 	    ICurveGauge(gauge).integrate_checkpoint();      // just check
-	    ICurveMinter(minter).token();                   // just check
 	    IERC20(reward).totalSupply();                   // just check
 	}
     
@@ -447,6 +447,59 @@ contract SFarm is IFarm, Governable {
         crop.safeApprove(pool, amount);
     }
     
+}
+
+
+contract TestGauge is SSimplePool, ICurveGauge, ICurveMinter {
+    address public reward;
+    
+	constructor(address _farm, address _underlying, address _reward) SSimplePool(_farm, _underlying) public {
+		initialize(msg.sender, _farm, _underlying, _reward);
+	}
+	
+	function initialize(address governor, address _farm, address _underlying, address _reward) public initializer {
+	    super.initialize(governor, _farm, _underlying);
+	    
+	    reward     = _reward;
+	    IERC20(reward).totalSupply();           // just check
+	}
+    
+    function deposit(uint _value) override external {
+        farming(msg.sender, _value);
+    }
+    function deposit(uint, address) override external {
+        require(false, 'no support deposit(uint, address)');
+    }
+    function withdraw(uint _value) override external {
+        unfarming(msg.sender, _value);
+    }
+    function withdraw(uint _value, bool claim_rewards) override external {
+        claim_rewards;
+        unfarming(msg.sender, _value);
+    }
+    function claim_rewards() override external {
+        reward.safeTransfer(msg.sender, 10);
+    }
+    function claim_rewards(address) override external {
+        require(false, 'no support claim_rewards(address)');
+    }
+    function claimable_reward(address addr) override external view returns (uint) {
+        addr;
+        return 10;
+    }
+    function claimable_tokens(address addr) override external view returns (uint) {
+        return harvestCapacity(addr)[1];
+    }
+    function integrate_checkpoint() override external view returns (uint) {
+        return now;
+    }
+    function token() override external view returns (address) {
+        return 0xD533a949740bb3306d119CC777fa900bA034cd52;      // CRV
+    }
+    function mint(address _gauge) override external {
+        _gauge;
+        harvest();
+    }
 }
 
 
